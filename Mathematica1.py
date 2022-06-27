758d048e76d612118a73bf3e912be1a87a7121ff
habeebatje <habeebo02@hotmail.com>
2022-06-27 11:50:57 -0400

    Update Mathematica1.py

--- Mathematica1.py
+++ Mathematica1.py
@@ -5,3 +5,13 @@ Created on Mon Jun 27 10:24:34 2022
 @author: habee
 """
 
+import numpy as np
+from numpy import transpose as tr
+from numpy import array as mat
+from numpy import linalg
+import matplotlib.pyplot as plt
+import random
+
+#Matrices
+
+A= mat([[0, mu, 0, 0], [0, 0, 0, 0], [0, 0, 0, mu], [0, 0, 0, 0]]);
