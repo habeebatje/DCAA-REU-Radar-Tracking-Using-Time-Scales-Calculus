14229dbc6c02f3d1b3f8bab7cf5f6b738cae7781
habeebatje <habeebo02@hotmail.com>
2022-06-29 12:45:22 -0400

    Update Mathematica1.py

--- Mathematica1.py
+++ Mathematica1.py
@@ -20,15 +20,16 @@ H= mat([[1,0,0,0],[0,0,1,0]])
 Covw= mat([[((mu**3)/3),((mu**2)/2),0,0], [((mu**2)/2),mu,0,0], [0,0,((mu**3)/3),((mu**2)/2)],[0,0,((mu**2)/2),mu]])
 # R= (1/mu)(mat([[0.001,0],[0,0.1]]))
 
-#*Set Up Time Scale*
+
+                                 #*Set Up Time Scale*
 
 tsl=1000
 #timescale limit
 MU= []
 TS= [0]
 
-xvals=mat([[0],[0],[0],[0]])
-yvals=mat([[0],[0]])
+xvals=mat([[[0],[0],[0],[0]]])
+yvals=mat([[[0],[0]]])
 
 
 i=1
@@ -44,17 +45,23 @@ while(i<=tsl):
     i+=1
     
 
-
-
 #For[i = 1, i <= tsl, i++, AppendTo[TS, Last[TS] + mu[[i]]]];
 i=1
 while(i<=tsl): 
-    listLen = len(TS)
     TS.append(TS[-1]+MU[i-1])   
     i+=1
     
-print(TS)
-
-
+                # (*Simulate the "True" state x and observations y*)
+                
+#For[i = 1, i <= tsl, i++, AppendTo[xvals, (IdentityMatrix[4] + A[mu[[i]]]) . Last[xvals] +
+#Transpose[{RandomReal[MultinormalDistribution[{0, 0, 0, 0}, Covw[mu[[i]]]]]}]]]
+i=1
+identityFour=np.zeros((4,4))
+randomMultinormalDist=np.random.multivariate_normal(mat[0,0,0,0], Covw[i])
+while(i<=tsl):
+    xvals.append((identityFour+A.dot(MU[i]).xvals[-1])+tr[randomMultinormalDist])
+    i+=1
+#figure this out at some point    
     
+print(xvals)
     
