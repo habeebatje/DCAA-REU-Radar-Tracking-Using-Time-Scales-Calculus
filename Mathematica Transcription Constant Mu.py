38b5f5709831644d4b9c7b951339615b64bee236
davfunk <108011450+davfunk@users.noreply.github.com>
2022-06-30 10:52:32 -0400

    Updated Version, includes fixes and Plotting

--- Mathematica Transcription Constant Mu.py
+++ Mathematica Transcription Constant Mu.py
@@ -12,7 +12,6 @@ from numpy import array as mat
 from numpy import linalg
 import matplotlib.pyplot as plt
 import scipy.stats as stats
-import random
 
 """
 Transcribing Mathematica
@@ -30,19 +29,25 @@ def R(mu):
     return (1/mu)*mat([[.001,0],[0,.1]])
 I = mat([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
 
+#Desired Values
+n=50
+
 tslist = [0]
 g = []
-for i in range (0,999):
+for i in range (0,n-1):
     mu = 1
     g.append(mu)
     tslist.append(tslist[i]+mu)
     i = i+1
 #ts = tsc.timescale(tslist)
 
+
 #Simulation
 xvals = [mat([[0],[0],[0],[0]])]
 zvals = [mat([[0],[0]])]
-for k in range (0,999):
+rsim = [0]
+tsim = [0]
+for k in range (0,n-1):
     mu = g[k]
     #system sim
     w1 = int(stats.norm.rvs(loc=0,scale=linalg.norm(Covw(g[k])),size=1))
@@ -51,13 +56,15 @@ for k in range (0,999):
     w4 = int(stats.norm.rvs(loc=0,scale=linalg.norm(Covw(g[k])),size=1))
     w = mat([[w1],[w2],[w3],[w4]])
     xnew = ((I + A(mu)).dot(xvals[k])) + w
+    rsim.append(xnew[0,0])
+    tsim.append(xnew[2,0])
     xvals.append(xnew)
-for k in range (0,998):
+for k in range (0,n-2):
     mu = g[k]
     v1 = int(stats.norm.rvs(loc=0,scale=linalg.norm(R(g[k])),size=1))
     v2 = int(stats.norm.rvs(loc=0,scale=linalg.norm(R(g[k])),size=1))
     v = mat([[v1],[v2]])
-    znew = H(mu).dot(xvals[k+1]) + tr(v)
+    znew = H(mu).dot(xvals[k+1]) + v
     zvals.append(znew)
 
 #Initialize
@@ -65,10 +72,13 @@ P = I
 Pminus = I
 x = mat([[0],[0],[0],[0]])
 xminus = mat([[0],[0],[0],[0]])
-eta = mat([[0]]) #Residual
+etar = [0] #Residual
+etat = [0]
+radius = [x[0,0]]
+theta = [x[2,0]]
 
 #Filter
-for k in range (0,999):  
+for k in range (0,n-1):  
     mu = g[k]
     #Error Propogation
     Pminus = (I + (A(mu).dot(P))).dot(tr(I + (A(mu)))) + Covw(mu)
@@ -77,3 +87,41 @@ for k in range (0,999):
     xminus = (I+A(mu)).dot(x)
     err = zvals[k] - H(mu).dot(xminus)
     x = xminus + ((P.dot(tr(H(mu)))).dot(linalg.inv(R(mu)))).dot(err)
+    r = x[0,0]
+    radius.append(r)
+    th = x[2,0]
+    theta.append(th)
+    etar.append(err[0,0])
+    etat.append(err[1,0])
+    
+#Plotting
+plt.figure(1)
+plt.plot(tslist,radius,'b^',tslist,rsim,'r^')
+#Blue is Estimation, Red is Sim
+plt.xlabel('iterations')
+plt.ylabel('radius values')
+plt.title('Radius Simulation v. Estimation')
+plt.grid(True)
+
+plt.figure(2)
+plt.plot(tslist,theta,'bo',tslist,tsim,'ro')
+#Blue is Estimation, Red is Sim
+plt.xlabel('iterations')
+plt.ylabel('angle values')
+plt.title('Angle Simulation v. Estimation')
+plt.grid(True)
+
+plt.figure(3)
+plt.plot(tslist,etar,'y-')
+plt.xlabel('iterations')
+plt.ylabel('Error')
+plt.title('Error for Radius')
+plt.grid(True)
+
+plt.figure(4)
+plt.plot(tslist,etat,'g-')
+plt.xlabel('iterations')
+plt.ylabel('Error')
+plt.title('Error for Angle')
+plt.grid(True)
+    
